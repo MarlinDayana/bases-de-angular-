c8ffa5580c649d12c47d3099de9f827ab63a6aff
MarlinDayana <florezmarlin1996@gmail.com>
2022-02-02 15:34:39 -0500

    cod fuente added

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,9 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+
+
+
+  // codigo fuente - va aqui - https://github.com/Klerith/angular-basicos/tree/v0.1.0
